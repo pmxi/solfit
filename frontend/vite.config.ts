ab90c8dfccb1bb47da672cb2ddf27225ac15a565
Paras Mittal <64939659+pmxi@users.noreply.github.com>
2026-05-18 08:44:12 -0400

    Use renamed repo path for Pages

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -20,7 +20,7 @@ export default defineConfig(({ mode }) => {
   const isProduction = mode === 'production'
 
   return {
-    base: isProduction ? '/starkhacks/' : '/',
+    base: isProduction ? '/solfit/' : '/',
     plugins: [
       figmaAssetResolver(),
       // The React and Tailwind plugins are both required for Make, even if
